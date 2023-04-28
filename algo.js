1f2758e122271c6b3b4ea3478c896e4eb2f87d3b
Alan Sarchman <alansarchman@live.com.ar>
2023-04-28 17:55:52 -0300

    actualizado 2

--- algo.js
+++ algo.js
@@ -4,21 +4,22 @@ const pEdadUsuario = document.querySelector("#edadUsuario")
 const edadUsuario = document.querySelector("#edad");
 const pInfoUsuario = document.querySelector("#infoUsuario");
 const emailUsuario = document.querySelector("#email")
-const pEmailUsuario = document.querySelector("#emailUsuario")
+const pEmailUsuario = document.querySelector("#emailUsuario");
 const dniUsuario = document.querySelector("#dni");
 const pDniUsuario = document.querySelector("#dniUsuario");
 const imagenComienzo = document.querySelector("#imagenComienzo");
-const imagenFinal = document.querySelector ("#imagenFinal")
+const imagenFinal = document.querySelector("#imagenFinal");
 const botonEnviar = document.querySelector("#boton");
+const contraseña = document.querySelector("#contraseña");
 const imgComienzo = "https://www.shutterstock.com/image-vector/spanish-language-vector-template-welcome-260nw-1792430023.jpg";
-const imgfinal = "https://t1.uc.ltmcdn.com/es/posts/6/7/3/como_decorar_la_entrada_de_la_casa_segun_el_feng_shui_25376_orig.jpg";
-
+const imgCorrecto = "https://tn.com.ar/resizer/JU-elW9Jkn9RmiUYUGwKwMqNDZQ=/767x0/smart/filters:format(webp)/cloudfront-us-east-1.images.arcpublishing.com/artear/5ZQ6IXDKONEMGR4CEIQKPSHA5M.jpg"
+const imgIncorrecto = "https://thumbs.dreamstime.com/b/mensaje-de-error-48389035.jpg"
 
 document.body.style.backgroundColor = "#0d1117";
 document.body.style.color = "#e6edf3";
 imagenComienzo.src = imgComienzo;
 
-function mensajeUsuario() {
+function mensajeUsuario(imagenMostrar, ingCon) {
     pInfoUsuario.innerHTML = ("Bienvenido a nuestra pagina " + nombreUsuario.value);
     pEmailUsuario.innerHTML = ("Su email es " + emailUsuario.value);
     pDniUsuario.innerHTML = ("Su dni es " + dniUsuario.value);
@@ -30,12 +31,9 @@ function mensajeUsuario() {
     pEdadUsuario.style.color = "pink";
     pEmailUsuario.style.color = "pink";
     pDniUsuario.style.color = "pink";
-    imagenComienzo.src = imgfinal;
+    imagenComienzo.src = imagenMostrar;
+    contraseña = ingCon
     if (edadUsuario.value >= 18) {
         pEdadUsuario.innerHTML = ("Su edad es de " + edadUsuario.value + " Es mayor de edad")
     } else pEdadUsuario.innerHTML = ("Su edad es de " + edadUsuario.value + " Es menor de edad")
-
 }
-
-
-
